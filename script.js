5cdde3978d63ea53e0d324df41e659ac037e2e9e
Yogesh <yogeshpatel.1505@gmail.com>
2020-09-05 00:48:05 +0530

    Added timer and popup

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let questions = [
     {
         question: "What does CSS stand for?",
@@ -29,68 +30,134 @@ let questions = [
 ];
 
 let userAns = [];
-
 $(document).ready(function () {
-    var currentIndex = 0;
+    var currentIndex = getCurrenIndex() ? getCurrenIndex() : 0;
     prepareTemplate(currentIndex);
 });
 
+function getCurrenIndex(params = 'currentQueIndex') {
+    return parseInt(window.localStorage.getItem(params));
+};
+
+
+function getTime(val) {
+    var valString = val + "";
+    if (valString.length < 2) {
+        return "0" + valString;
+    } else {
+        return valString;
+    }
+}
+
+function startTimer(sec) {
+
+    this.timer = setTimeout(() => {
+
+        document.getElementById('time').innerHTML = `<span>${getTime(parseInt(sec / 60))}</span>:<span>${getTime(sec % 60)}</span>`;
+        if (sec > 0) {
+            window.localStorage.setItem('currentTime', sec);
+            startTimer(sec - 1);
+        } else {
+            window.localStorage.removeItem('currentTime');
+            let currentIndex = getCurrenIndex() + 1;
+            prepareTemplate(currentIndex);
+        }
+
+    }, 1000);
+}
+
 
 function prepareTemplate(index) {
-    const currentQuestion = questions[index];
 
-    document.getElementById('questionTitle').innerHTML = currentQuestion.question;
+    if (questions.length > index) {
+        clearTimeout(this.timer);
+        window.localStorage.setItem('currentQueIndex', index);
+        const currentQuestion = questions[index];
 
-    const answer = currentQuestion.answers;
+        document.getElementById('questionTitle').innerHTML = currentQuestion.question;
 
-    const answersKeys = Object.keys(currentQuestion.answers);
+        const answer = currentQuestion.answers;
 
-    let template = '';
+        const answersKeys = Object.keys(currentQuestion.answers);
 
-    answersKeys.forEach((ans, i) => {
-        template += `<li class="list-group-item">
-                        <div class="custom-control custom-radio">
-                            <input type="radio" id="answer${i + 1}" name="answer" value="${ans}" class="custom-control-input">
-                            <label class="custom-control-label" for="answer${i + 1}">${answer[ans]}</label>
-                        </div>
-                    </li>`;
-    });
+        let template = '';
 
-    template += `<li class="list-group-item"><button type="button" class="btn btn-primary" onclick="displayRadioValue(${index + 1})">Submit</button></li>`;
+        answersKeys.forEach((ans, i) => {
+            template += `<li class="list-group-item">
+                            <div class="custom-control custom-radio">
+                                <input type="radio" id="answer${i + 1}" name="answer" value="${ans}" class="custom-control-input">
+                                <label class="custom-control-label" for="answer${i + 1}">${answer[ans]}</label>
+                            </div>
+                        </li>`;
+        });
 
-    document.getElementById('quizwizard').innerHTML = template;
+        template += `<li class="list-group-item"><button type="button" class="btn btn-primary" onclick="displayRadioValue(${index + 1})">Submit</button></li>`;
+
+        document.getElementById('quizwizard').innerHTML = template;
+        let currentTime = getCurrenIndex('currentTime') ? getCurrenIndex('currentTime') : 10;
+        startTimer(currentTime);
+    } else {
+        resetForm();
+        window.localStorage.removeItem('currentQueIndex');
+
+    }
 
 }
 
 
 function displayRadioValue(nextIndex) {
-
+    window.localStorage.removeItem('currentTime');
     let selectedQuestion = questions[nextIndex - 1];
 
     var options = document.getElementsByName('answer');
 
     let selectedAnd = false;
-
+    let isChecked = false;
     for (i = 0; i < options.length; i++) {
         if (options[i].checked) {
+            isChecked = true;
             selectedAnd = options[i];
         }
     }
-
-    if (selectedQuestion.correctAnswer === selectedAnd.value) {
-        console.log("Options are correct : ");
-    } else {
-        console.log('Wrong answer');
+    if (!isChecked) {
+        $("#myModal").modal('show');
+        return;
     }
+    selectedQuestion.isCorrect = selectedQuestion.correctAnswer === selectedAnd.value ? true : false;
+
     selectedQuestion.userOption = selectedAnd.value;
     userAns.push(selectedQuestion);
 
     if (nextIndex >= questions.length) {
-        console.log("User Answers :",userAns);
-       let successTemplate = `<li class="list-group-item"><button type="button" class="btn btn-info" onclick="displayResult()">Show Quiz Result</button></li>`;
-
-        document.getElementById('quizwizard').innerHTML = "<b>Question is submitted successfully</b><br/>" + successTemplate;
+       
+        resetForm();
+        
     } else {
         prepareTemplate(nextIndex);
     }
-} 
+}
+
+function resetForm(){
+    clearTimeout(this.timer);
+    document.getElementById('time').innerHTML = "";
+    document.getElementById('questionTitle').innerHTML = "";
+    window.localStorage.removeItem('currentQueIndex');
+
+    console.log("User Answers :", userAns);
+    let successTemplate = `<li class="list-group-item"><button type="button" class="btn btn-info" onclick="displayResult()">Show Quiz Result</button></li>`;
+    document.getElementById('quizwizard').innerHTML = successTemplate;
+}
+
+function displayResult() {
+
+    let attemptQue = userAns.length;
+   let correctAnswer = userAns.filter((item)=>item.isCorrect).length;
+   let totalQue =  questions.length;
+   console.log("attemptQue",attemptQue);
+   console.log("correctAnswer",correctAnswer);
+   document.getElementById('totalQue').innerHTML = totalQue;
+   document.getElementById('attemptQue').innerHTML = attemptQue;
+   document.getElementById('correctAns').innerHTML = correctAnswer;
+
+   $("#showResult").modal('show');
+};
